ffd9f0a6633dd525ce13f25aba547036d595a98a
Aaron Winter <winter.aaron93@gmail.com>
2021-03-21 14:57:27 +0100

    brittle wall fix

--- src/interactable/brittle/BrittleWall.gd
+++ src/interactable/brittle/BrittleWall.gd
@@ -2,12 +2,16 @@ extends Interactable
 
 var _is_broken := false
 
+var _stage := 0
 
 func _interact(__: Character) -> void:
-	if not _is_broken:
+	if _stage == 0:
+		_stage = 1
+		sprite.frame = 1
+	elif _stage == 1:
+		_stage = 2
 		anim.play("break")
-		_is_broken = true
 
 
 func _is_blocking(__: Array) -> bool:
-	return not _is_broken
+	return not _stage == 2
